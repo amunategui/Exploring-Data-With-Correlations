b663659cdb9b9000f253465136f63b1bfe3cdd93
Manuel Amunategui <amunategui@gmail.com>
2014-09-29 23:01:07 -0700

    uploading site code

---
+++ Correlations.R
@@ -0,0 +1,53 @@
+library(RCurl) # download https data
+urlfile <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'
+x <- getURL(urlfile, ssl.verifypeer = FALSE)
+adults <- read.csv(textConnection(x), header=F)
+
+# if the above getURL command fails, try this:
+# adults <-read.csv('https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data', header=F)
+
+names(adults)=c('age','workclass','fnlwgt','education','educationNum',
+                'maritalStatus','occupation','relationship','race',
+                'sex','capitalGain','capitalLoss','hoursWeek',
+                'nativeCountry','income')
+
+adults$income <- ifelse(adults$income==' <=50K',1,0)
+
+library(caret)
+dmy <- dummyVars(" ~ .", data = adults)
+adultsTrsf <- data.frame(predict(dmy, newdata = adults))
+
+## Correlation matrix with p-values. See http://goo.gl/nahmV for documentation of this function
+cor.prob <- function (X, dfr = nrow(X) - 2) {
+        R <- cor(X, use="pairwise.complete.obs")
+        above <- row(R) < col(R)
+        r2 <- R[above]^2
+        Fstat <- r2 * dfr/(1 - r2)
+        R[above] <- 1 - pf(Fstat, 1, dfr)
+        R[row(R) == col(R)] <- NA
+        R
+}
+
+## Use this to dump the cor.prob output to a 4 column matrix
+## with row/column indices, correlation, and p-value.
+## See StackOverflow question: http://goo.gl/fCUcQ
+flattenSquareMatrix <- function(m) {
+        if( (class(m) != "matrix") | (nrow(m) != ncol(m))) stop("Must be a square matrix.")
+        if(!identical(rownames(m), colnames(m))) stop("Row and column names must be equal.")
+        ut <- upper.tri(m)
+        data.frame(i = rownames(m)[row(m)[ut]],
+                   j = rownames(m)[col(m)[ut]],
+                   cor=t(m)[ut],
+                   p=m[ut])
+}
+
+corMasterList <- flattenSquareMatrix (cor.prob(adultsTrsf))
+print(head(corMasterList,10))
+
+corList <- corMasterList[order(corMasterList$cor),]
+selectedSub <- subset(corList, (abs(cor) > 0.2 & j == 'income'))
+bestSub <-  sapply(strsplit(as.character(selectedSub$i),'[.]'), "[", 1)
+bestSub <- unique(bestSub)
+
+library(psych)
+pairs.panels(adults[c(bestSub, 'income')])
